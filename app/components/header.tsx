e8508271059f894264bd784340809c9180151437
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-06-26 16:29:08 -0300

    Batata

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,6 +2,7 @@
 
 import styled from "styled-components"
 import Link from "next/link"
+import {GiPotato} from "react-icons/gi"
 
 
 
@@ -9,6 +10,7 @@ export default function Header(){
     return (
         <Wrapper>
             <Link href={"/"}>Home</Link>
+            <Link href={"/"}><GiPotato/></Link>
             <Link href={"/about"}>About</Link>
         </Wrapper>
     )
@@ -16,9 +18,22 @@ export default function Header(){
 
 const Wrapper = styled.div `
     display: flex;
+    align-items: center;
     justify-content: space-between;
     gap: 5%;
-    transition: all 0.5s;
+
+    svg{
+        fill: #000000;
+        background-color: none;
+        height: 1.5em;
+        width: 2.5em;
+        transition: 0.5s;
+        &:hover{
+        fill: white;
+        }
+
+    }
+    
     background-color: white;
     a{
         text-decoration: none;
@@ -27,6 +42,7 @@ const Wrapper = styled.div `
         padding: 1em 2em;
         font-weight: bold;
         font-size: 1.5em;
+        transition: all 0.5s;
     &:hover {
         background-color: black;
         color: white;
