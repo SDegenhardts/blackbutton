cde71f0ec7fd7b89ff8bf71d1283834abd6b9481
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-07-04 16:38:17 -0300

    button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import styled from "styled-components"
 import { useState } from "react"
 import Link from "next/link"
+import { GiCelebrationFire } from "react-icons/gi"
 
 // export const metadata = {
 //   title: 'About',
@@ -31,6 +32,7 @@ export default function Home() {
     <div>
       <Image src={"/fritadeira.png"} width={50} height={50} alt={"frita"}/>
       <Image src={"/ralador.png"} width={50} height={50} alt={"ralar"}/>
+      <GiCelebrationFire/>
     </div>
       <Image onClick={() => imagem == image[image.length -1]?setImage(image[0]):setImage(image[image.indexOf(imagem)+1])} src={imagem} width={1000} height={500} alt='Batata'/>
     </Wrapper>
